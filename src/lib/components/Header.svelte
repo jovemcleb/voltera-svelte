c450d088094fa0d73df38d9a48dae343a4216372
jovemcleb <lima.cardoso.caleb@gmail.com>
2024-05-01 14:55:47 -0300

    feat: adds header in app layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Logo from "./Logo.svelte";
+
+  const navLinks = [
+    { href: "/markdown", label: "Markdown" },
+    { href: "/america", label: "América" },
+    { href: "/tipografia", label: "Tipografia" },
+  ];
+</script>
+
+<header class="header">
+  <Logo />
+  <nav>
+    <ul>
+      {#each navLinks as { href, label }}
+        <li>
+          <a class="link" {href}>{label}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+</header>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #48a715;
+    padding: 1rem 2rem;
+  }
+
+  nav ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+  }
+
+  nav ul li {
+    margin: 0 1rem;
+  }
+
+  .link {
+    color: white;
+    text-decoration: none;
+  }
+
+  .link:hover {
+    text-decoration: underline;
+  }
+</style>
