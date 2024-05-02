521c114a17e453e435ef70de3edfaf0e3e9af828
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-05-02 18:00:57 -0300

    feat: responsiveness in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,12 +7,20 @@
     { href: "/america", label: "América" },
     { href: "/tipografia", label: "Tipografia" },
   ];
+
+  let isPopoverOpen = false;
+
+  function togglePopover() {
+    isPopoverOpen = !isPopoverOpen;
+  }
 </script>
 
 <header class="header">
   <div class="content">
-    <Logo />
-    <nav>
+    <a href="/">
+      <Logo />
+    </a>
+    <nav class:open={isPopoverOpen}>
       <ul>
         {#each navLinks as { href, label }}
           <li>
@@ -21,7 +29,28 @@
         {/each}
       </ul>
     </nav>
+    <button class="popover-trigger" on:click={togglePopover}>
+      <span class="hamburger">
+        {#if isPopoverOpen}
+          ✖
+        {:else}
+          ☰
+        {/if}
+      </span>
+    </button>
   </div>
+
+  {#if isPopoverOpen}
+    <div class="popover open">
+      <ul>
+        {#each navLinks as { href, label }}
+          <li>
+            <a class="link" {href} on:click={togglePopover}>{label}</a>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/if}
 </header>
 
 <style>
@@ -57,5 +86,70 @@
 
   .link:hover {
     text-decoration: underline;
+  }
+
+  .popover {
+    position: absolute;
+    top: 6rem;
+    right: 0;
+    width: 100%;
+    background-color: #f0f2f5;
+    padding: 0.5rem;
+    display: none;
+    z-index: 999;
+    border-radius: 0 0 0.5rem 0.5rem;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.1),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  .popover.open {
+    display: block;
+  }
+
+  .popover ul {
+    list-style: none;
+    padding: 0 1rem;
+  }
+
+  .popover li {
+    margin: 0.75rem 0;
+    text-align: center;
+  }
+
+  .popover .link {
+    color: black;
+    font-size: 1.4rem;
+    text-decoration: none;
+  }
+
+  .popover .link:hover {
+    text-decoration: underline;
+  }
+
+  .popover-trigger {
+    background: none;
+    border: none;
+    cursor: pointer;
+    display: none;
+  }
+
+  .hamburger {
+    font-size: 2rem;
+    color: white;
+  }
+
+  @media screen and (max-width: 769px) {
+    .content {
+      padding: 1rem;
+    }
+
+    .content nav {
+      display: none;
+    }
+
+    .popover-trigger {
+      display: block;
+    }
   }
 </style>
