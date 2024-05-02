00a5a694f7736dfec84abc32ade4609e43f2bce4
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-05-02 10:11:16 -0300

    feat: adds america route

---
+++ src/routes/america/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { convertMarkdownToHtml } from "$lib/utils/convertMarkdownToHtml";
+  import { onMount } from "svelte";
+
+  export let data;
+  const markdownContent = data.markdownContent;
+  let htmlSafe = "";
+
+  onMount(async () => {
+    htmlSafe = await convertMarkdownToHtml(markdownContent);
+  });
+</script>
+
+<div class="content">{@html htmlSafe}</div>
